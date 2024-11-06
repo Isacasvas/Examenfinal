e613aae7be1b8e30e9fd392829a1e8fbe2cf982a
Isacasvas <jcastillov10@miumg.edu.gt>
2024-11-06 13:33:33 -0600

    Cambiosfinales

--- app/config/env.js
+++ app/config/env.js
@@ -1,10 +1,15 @@
 const env = {
-  database: 'basefinal',
-  username: 'basefinal_user',
-  password: 'xEaWYluoSVpfA59VeI20nP1TPdwO1rAB',
-  host: 'dpg-cslrt252ng1s73be809g-a.oregon-postgres.render.com',
-  dialect: 'postgres',
-  ssl: true,
+  database: 'basefinal', // Nombre de la base de datos
+  username: 'basefinal_user', // Nombre de usuario
+  password: 'xEaWYluoSVpfA59VeI20nP1TPdwO1rAB', // Contraseña
+  host: 'dpg-cslrt252ng1s73be809g-a.oregon-postgres.render.com', // Host de la base de datos
+  dialect: 'postgres', // Motor de la base de datos
+  dialectOptions: {
+      ssl: {
+          require: true, // Habilita SSL
+          rejectUnauthorized: false // Permite conexiones SSL sin verificación de CA
+      }
+  },
   pool: {
       max: 5,
       min: 0,
